8ab602cd8863677fec4e062579328fa6fda7b92d
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-09-26 07:25:57 -0300

    typography styles added to the application

---
+++ src/Styles/theme.ts
@@ -0,0 +1,50 @@
+const theme = {
+  colors: {
+    blue_main: {
+      primary_normal: "#1B6CE7",
+    primary_dark: "#155ECD",
+    primary_light: "#4589F0",
+    secondary_normal: "#0055D6",
+    secondary_dark: "#004ABF",
+    secondary_light: "#3376E3",
+    },
+    white_auxiliary: {
+      white_normal: "#F6FAFF",
+      white_dark: "#F1F7FF",
+      white_light: "#F8FBFF"
+    },
+    black_auxiliary: {
+      black_normal: "#242526",
+      black_dark: "#18191A",
+      black_light: "#303133"
+    },
+    gray_auxiliary: {
+      gray_1: "#E6E6E7",
+      gray_2: "#CDCDCE",
+      gray_3: "#B5B5B6",
+      gray_4: "#9C9C9D",
+      gray_5: "#838385",
+      gray_6: "#737374",
+      gray_7: "#626264",
+    },
+    red_auxiliary: {
+      red_normal: '#eb3d3d',
+      red_dark: '#D03333',
+      red_light: '#F05454'
+    },
+    green_auxiliary: {
+      green_normal: '#32d957',
+      green_dark: '#28bb49',
+      green_light: '#50e170'
+    },
+    yellow_auxiliary: {
+        yellow_normal: '#ffce52',
+        yellow_dark: '#d9b043',
+        yellow_light: '#ffd76b'
+    }
+    
+  },
+  
+};
+
+export default theme;
