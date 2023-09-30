552e31612bf59fba9c1c18b401e8b2e00ec5aaac
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-09-30 06:36:16 -0300

    header structure implemented

---
+++ src/Components/Login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledLogin } from "../Styles/Login.styled";
+
+const Login = () => {
+  return <StyledLogin>Login</StyledLogin>;
+};
+
+export default Login;
---
+++ src/Styles/Header.styled.ts
@@ -0,0 +1,8 @@
+import styled, {css} from "styled-components";
+
+export const StyledHeader = styled.header`
+${({theme}) => css `
+
+
+`}
+`
