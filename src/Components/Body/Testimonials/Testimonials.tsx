f8692d87100dc54af68eccc7c43636e61360418e
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-11-06 08:12:43 -0300

    add last touches to testimonials

--- src/Components/Body/Testimonials/Testimonials.tsx
+++ src/Components/Body/Testimonials/Testimonials.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ContentTestimonials } from '../../../Contents';
 
-import { Pagination } from 'swiper/modules';
+import {Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { StyledTestimonials } from '../../../Styles/Body/Testimonials/Testimonials.styled';
 
@@ -10,28 +10,42 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { StyledHeadings } from '../../../Styles/Reusable/Headings.styled';
+import ContainerSizes from '../../../ContainerSizes';
+import { StyledTexts } from '../../../Styles/Reusable/Texts.styled';
 
 const Testiominials = () => {
+  const {xsmall} = ContainerSizes();
+
   return (
     <StyledTestimonials className='container'>
-
       <div className='box'>
-        <StyledHeadings className='testimonials-subtitle' $device='mobile' as='h5'>{ContentTestimonials.h5_subtitle}</StyledHeadings>
-        <StyledHeadings className='testimonials-title' $device='mobile' as='h2'>{ContentTestimonials.h2_title}</StyledHeadings>
+        <StyledHeadings
+          className='testimonials-subtitle'
+          $device='mobile'
+          as='h5'
+        >
+          {ContentTestimonials.h5_subtitle}
+        </StyledHeadings>
+        <StyledHeadings className='testimonials-title' $device='mobile' as='h2'>
+          {ContentTestimonials.h2_title}
+        </StyledHeadings>
       </div>
 
       <Swiper
-        modules={[Pagination]}
+        modules={[Pagination, Navigation]}
         spaceBetween={40}
-        slidesPerView={1}
+        slidesPerView={xsmall ? 1 : 2}
+        navigation
         grabCursor={true}
         pagination={{ clickable: true, type: 'progressbar' }}
       >
-        
-            <SwiperSlide>
-              
-            </SwiperSlide>
-         
+        {ContentTestimonials.list_testimonials.map(({name, testimonial, photo, id}) => {
+          return <SwiperSlide key={id}>
+            <img src={photo} className='photo-profile'></img>
+            <StyledHeadings className='testimonials-name' $device="mobile" as="h4">{name}</StyledHeadings>
+            <StyledTexts $device='mobile' $size='p1'>{testimonial}</StyledTexts>
+          </SwiperSlide>;
+        })}
       </Swiper>
     </StyledTestimonials>
   );
--- src/Contents.tsx
+++ src/Contents.tsx
@@ -5,6 +5,13 @@ import Transfers from './assets/svg/icon-transfer.svg?react';
 import Card from './assets/svg/icon-card.svg?react';
 import Support from './assets/svg/icon-support.svg?react';
 
+import Woman1 from './assets/img/testimonial-woman-1.jpg';
+import Woman2 from './assets/img/testimonial-woman-2.jpg';
+import Woman3 from './assets/img/testimonial-woman-3.jpg';
+
+import Man1 from './assets/img/testimonial-man-1.jpg';
+import Man2 from './assets/img/testimonial-man-2.jpg';
+
 export const ContentHero = {
   title: 'Welcome to Supreme Bank: Your financial future starts here.',
   paragraph:
@@ -63,10 +70,35 @@ export const ContentTestimonials = {
   h5_subtitle: 'TESTIMONIALS',
   h2_title: 'Discover the Supreme Experience',
   list_testimonials: [
-    { photo: '', name: '', testimonial: '' },
-    { photo: '', name: '', testimonial: '' },
-    { photo: '', name: '', testimonial: '' },
-    { photo: '', name: '', testimonial: '' },
-    { photo: '', name: '', testimonial: '' },
+    { id: 1,
+      photo: Woman1,
+      name: 'Joanne Fuentes',
+      testimonial:
+        "I recently moved to a new city and was in search of a trustworthy bank. I'm so glad I found Supreme Bank. I appreciate the personalized attention they give to their customers.",
+    },
+    {id: 2,
+      photo: Woman3,
+      name: 'Kelly Davis',
+      testimonial:
+        "I've been their customer for years, and I couldn't be happier with their services. Their friendly staff is always there to assist me with any questions.",
+    },
+    { id: 3,
+      photo: Man1,
+      name: 'Harry Portman',
+      testimonial:
+        'The security and peace of mind that they offer are unparalleled. I never have to worry about my accounts or personal information being compromised. Thank you Supreme Bank.',
+    },
+    { id: 4,
+      photo: Woman2,
+      name: 'Lucy Young',
+      testimonial:
+        "I appreciate Sup's commitment to community involvement. Their charitable initiatives and sponsorships show their dedication to making a positive impact in our local area. It's a bank that truly cares about the community it serves.",
+    },
+    { id: 5,
+      photo: Man2,
+      name: 'Norman Smith',
+      testimonial:
+        "I've had accounts with several banks, but Supreme stands out from the rest. Their commitment to security and innovation is impressive. I feel confident knowing my money is in safe hands.",
+    },
   ],
 };
