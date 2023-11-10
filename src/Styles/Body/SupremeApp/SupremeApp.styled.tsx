9e08d8c78b054f54f09bc2d1fbf684b15c8f8211
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-11-09 23:00:54 -0300

    add last touches in bullets from app section

--- src/Styles/Body/SupremeApp/SupremeApp.styled.tsx
+++ src/Styles/Body/SupremeApp/SupremeApp.styled.tsx
@@ -1,24 +1,39 @@
 import styled, { css } from 'styled-components';
 
-interface SupremeAppProps {
-
-}
+interface SupremeAppProps {}
 
 export const StyledSupremeApp = styled.section<SupremeAppProps>`
   ${({ theme }) => css`
-    .SupremeApp-item-1 {
+    .supremeapp-item-1 {
       ${theme.boxProps.flex_items.flex_item_1};
-      height: 100vh;
-      
+
+      .title {
+        ${theme.bullets.title};
+      }
+
+      .item-1 {
+        background: red;
+        height: 100px;
+      }
+
+      .supremeapp-list-item {
+        ${theme.bullets.list_item};
+      }
+
+      .list-wrapper {
+        ${theme.bullets.list_wrapper};
+      }
+
+      .bullet-icons {
+        ${theme.bullets.bullet_icons};
+      }
+
+      .supremeapp-list {
+        ${theme.bullets.list};
+      }
 
       .bullet-title {
-        background: ${theme.colors.black_auxiliary.black_normal};
-        
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        font-weight: bold;
-        padding-bottom: 8px;
-        
+        ${theme.bullets.bullet_title};
       }
     }
   `}
