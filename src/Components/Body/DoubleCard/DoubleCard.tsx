21d2f4d42b0386a4461d3358d95e0ec4f4c08186
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-11-26 10:00:36 -0300

    feat: add more features to double card section

--- src/Components/Body/DoubleCard/DoubleCard.tsx
+++ src/Components/Body/DoubleCard/DoubleCard.tsx
@@ -1,23 +1,53 @@
-import React from 'react'
-import { StyledDoubleCard } from '../../../Styles/Body/DoubleCard/DoubleCard.styled'
+import React from 'react';
+import { StyledDoubleCard } from '../../../Styles/Body/DoubleCard/DoubleCard.styled';
 import { StyledHeadings } from '../../../Styles/Reusable/Headings.styled';
 import { StyledTexts } from '../../../Styles/Reusable/Texts.styled';
+import { ContentDoubleCard } from '../../../Contents';
+import Icons from '../../Reusable/Icons';
 
 interface DoubleCardProps {
   smallState: 'mobile' | 'desktop';
 }
 
-const DoubleCard = ({smallState}: DoubleCardProps ) => {
+const DoubleCard = ({ smallState }: DoubleCardProps) => {
   return (
     <StyledDoubleCard className='container'>
       <div className='double-card-flex-item-1'>
-        <StyledHeadings as='h5' $device={smallState}>TURN SUPREME</StyledHeadings>
-        <StyledHeadings as='h2' $device={smallState}>Credit and debit card.</StyledHeadings>
-        <StyledTexts $size='p1' $device={smallState}></StyledTexts>
+        <StyledHeadings as='h5' $device={smallState}>
+          {ContentDoubleCard.h5_subtitle}
+        </StyledHeadings>
+        <StyledHeadings as='h2' $device={smallState} className='title'>
+          {ContentDoubleCard.h2_title}
+        </StyledHeadings>
+        <StyledTexts $size='p1' $device={smallState}>
+          {ContentDoubleCard.p_description}
+        </StyledTexts>
+
+        <ul className='list'>
+          {ContentDoubleCard.icons_bullets.map(
+            ({ bullet_id, bullet_title, bullet_description, bullet_icon }) => {
+              return (
+                <li key={bullet_id} className='list-wrapper'>
+                  <Icons className='bullet-icons'>{bullet_icon}</Icons>
+                  <div className='list-item'>
+                    <StyledHeadings $device={smallState} as='h5' className='bullet-title'>
+                      {bullet_title}
+                    </StyledHeadings>
+                    <StyledTexts $device={smallState} $size='p4'>{bullet_description}</StyledTexts>
+                  </div>
+                </li>
+              );
+            }
+          )}
+        </ul>
+
+        <StyledTexts as='a' href='' $size='p1' $device={smallState} $link={true}>
+          {ContentDoubleCard.link_double_card}
+        </StyledTexts>
       </div>
       <div className='double-card-flex-item-1'></div>
     </StyledDoubleCard>
-  )
-}
+  );
+};
 
-export default DoubleCard
+export default DoubleCard;
--- src/Styles/Body/DoubleCard/DoubleCard.styled.tsx
+++ src/Styles/Body/DoubleCard/DoubleCard.styled.tsx
@@ -1,8 +1,30 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components';
 
 export const StyledDoubleCard = styled.section`
-  ${({theme}) => css`
-    
-  `}
+  ${({ theme }) => css`
 
-`
+  .title {
+    ${theme.bullets.title};
+  }
+
+    .bullet-icons {
+      ${theme.bullets.bullet_icons}
+    }
+
+    .bullet-title {
+      ${theme.bullets.bullet_title};
+    }
+
+    .list-wrapper {
+      ${theme.bullets.list_wrapper}
+    }
+
+    .list-item {
+      ${theme.bullets.list_item};
+    }
+
+    .list {
+      ${theme.bullets.list};
+    }
+  `}
+`;
