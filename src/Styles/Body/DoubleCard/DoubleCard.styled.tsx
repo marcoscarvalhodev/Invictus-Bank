68d41521c5935bcfb393c5ba2104b2c4d632f4e0
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-11-30 00:34:43 -0300

    feat: add last styles and motion to double cards

--- src/Styles/Body/DoubleCard/DoubleCard.styled.tsx
+++ src/Styles/Body/DoubleCard/DoubleCard.styled.tsx
@@ -47,18 +47,77 @@ export const StyledDoubleCard = styled.section<DoubleCardsProps>`
 
     .double-cards {
       width: 500px;
+      height: 500px;
     }
 
     .double-cards-wrapper {
       padding: 24px;
       display: flex;
       justify-content: center;
-      align-items: center;
+      
     }
 
     ${$small && css`
     display: flex;
       flex-direction: column;
     `}
+
+
+    #double-card-verse {
+      transform-origin: center center;
+      transform-box: fill-box;
+      transform: translate3d(0,-40px,0) rotate(0deg);
+      animation: double-card-verse-move 3s ease infinite alternate;
+    }
+
+    @keyframes double-card-verse-move {
+      0% {
+        transform: translate3d(0,-40px,0) rotate(0deg);
+      }
+
+      100% {
+        transform: translate3d(0,20px,0) rotate(5deg);
+      }
+    }
+
+
+    #double-card-front {
+      transform-origin: center center;
+      transform-box: fill-box;
+      transform: translate3d(0,-40px,0) rotate(0deg);
+      animation: double-card-front-move 3s ease 1s infinite alternate;
+    }
+
+    @keyframes double-card-front-move {
+      0% {
+        transform: translate3d(0,-40px,0) rotate(0deg);
+      }
+
+      100% {
+        transform: translate3d(0,20px,0) rotate(-5deg);
+      }
+    }
+
+    #double-card-shadow {
+      transform-origin: center center;
+      transform-box: fill-box;
+      fill-opacity: 0.15;
+      animation: double-card-shadow 3s ease 1s infinite alternate;
+    }
+
+
+    @keyframes double-card-shadow {
+      0% {
+        fill-opacity: 0.15;
+      }
+
+      100% {
+        fill-opacity: 0.4;
+      }
+    }
+
+
   `}
+
+
 `;
