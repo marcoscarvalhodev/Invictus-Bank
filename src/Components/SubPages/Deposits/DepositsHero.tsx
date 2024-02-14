4bfcc379e77a2421c1a3a6417aa92159ee1f42f9
Marcos Carvalho <marcospaulocs633@gmail.com>
2024-02-13 23:28:40 -0300

    feat: add motion to safe in deposits section

--- src/Components/SubPages/Deposits/DepositsHero.tsx
+++ src/Components/SubPages/Deposits/DepositsHero.tsx
@@ -4,6 +4,9 @@ import { StyledHeadings } from '../../../Styles/Reusable/Headings.styled';
 import { StyledTexts } from '../../../Styles/Reusable/Texts.styled';
 
 import Safe from '../../../assets/svg/deposits/safe.svg?react';
+import SafeDoor from '../../../assets/svg/deposits/safe-door.svg?react';
+import SafeShadows from '../../../assets/svg/deposits/safe-shadows.svg?react';
+import ShadowWrapper from '../../../assets/svg/deposits/shadow-wrapper.svg?react';
 
 interface DepositsHeroProps {
   smallState: 'mobile' | 'desktop';
@@ -13,12 +16,27 @@ const DepositsHero = ({ smallState }: DepositsHeroProps) => {
   return (
     <StyledDepositsHero className='container'>
       <div className='flex-hero'>
-        <StyledHeadings as='h5' $device={smallState}>Subtitulo</StyledHeadings>
-        <StyledHeadings as='h2' $device={smallState} className='title-hero'>Titulo</StyledHeadings>
-        <StyledTexts $size='p1' $device={smallState}>Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição Descrição</StyledTexts>
+        <StyledHeadings as='h5' $device={smallState}>
+          Subtitulo
+        </StyledHeadings>
+        <StyledHeadings as='h2' $device={smallState} className='title-hero'>
+          Titulo
+        </StyledHeadings>
+        <StyledTexts $size='p1' $device={smallState}>
+          Descrição Descrição Descrição Descrição Descrição Descrição Descrição
+          Descrição Descrição Descrição Descrição Descrição Descrição Descrição
+          Descrição
+        </StyledTexts>
       </div>
       <div className='flex-hero flex-hero-2'>
-        <Safe className='safe'/>
+        <div className='safe-wrapper'>
+          <Safe className='safe' />
+          
+            <SafeDoor className='safe-door' />
+            <SafeShadows className='safe-shadows' />
+            <ShadowWrapper className='shadow-wrapper' />
+          
+        </div>
       </div>
     </StyledDepositsHero>
   );
